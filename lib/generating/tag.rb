761976e6bdc06ea98189c57a02b7a42c363584f4
Semeikin_Kirill <semeikinkirill21@gmail.com>
2023-02-25 21:06:31 +0300

    added generating tags

---
+++ lib/generating/tag.rb
@@ -0,0 +1,25 @@
+module HexletCode
+    module Tag
+
+        @@single_tags_list = %w[link input meta param source track wbr keygen area base br col embed hr img]
+      
+        def self.build(tag_name, attributes = {})
+          text_attributes = attribute_generation attributes
+      
+          if @@single_tags_list.include?(tag_name)
+              "<#{tag_name}#{text_attributes}>"
+          else
+              text = block_given? ? yield : ''
+              "<#{tag_name}#{text_attributes}>#{text}</#{tag_name}>"
+          end
+        end
+      
+        def self.attribute_generation(attributes)
+          return '' if attributes.empty?
+      
+          result = attributes.to_a.map { |key, value| " #{key}=\"#{value}\"" }
+          result.join
+        end
+      
+    end
+end
--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,5 +4,5 @@ require_relative "hexlet_code/version"
 
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
+  autoload :Tag, './generating/tag'
 end
