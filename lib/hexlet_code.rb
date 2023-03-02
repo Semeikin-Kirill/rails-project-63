868d709dad74159f74b4e10c0223a9c0b6baecb4
Semeikin_Kirill <semeikinkirill21@gmail.com>
2023-03-02 11:37:23 +0300

    fix badge

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -3,19 +3,20 @@
 require_relative 'hexlet_code/version'
 # root module
 module HexletCode
-  class Error < StandardError; end
   autoload :Tag, 'hexlet_code/tag.rb'
   autoload :FormCreate, 'hexlet_code/form.rb'
 
-  def self.form_for(user, url: '#', **options)
-    attributes_form = { action: url, method: 'post' }.merge(options)
-    Tag.build('form', attributes_form) do
-      f = FormCreate.new user
-      value = yield(f) if block_given?
-      if f.result.empty?
-        value
-      else
-        f.result
+  class << self
+    def form_for(user, url: '#', **options)
+      attributes_form = { action: url, method: 'post' }.merge(options)
+      Tag.build('form', attributes_form) do
+        f = FormCreate.new user
+        value = yield(f) if block_given?
+        if f.result.empty?
+          value
+        else
+          f.result
+        end
       end
     end
   end
--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -5,22 +5,24 @@ module HexletCode
   module Tag
     @single_tags_list = %w[link input meta param source track wbr keygen area base br col embed hr img]
 
-    def self.build(tag_name, attributes = {})
-      text_attributes = attribute_generation attributes
-
-      if @single_tags_list.include?(tag_name)
-        "<#{tag_name}#{text_attributes}>"
-      else
-        result = block_given? ? yield : ''
-        "<#{tag_name}#{text_attributes}>#{result}</#{tag_name}>"
+    class << self
+      def build(tag_name, attributes = {})
+        text_attributes = attribute_generation attributes
+  
+        if @single_tags_list.include?(tag_name)
+          "<#{tag_name}#{text_attributes}>"
+        else
+          result = block_given? ? yield : ''
+          "<#{tag_name}#{text_attributes}>#{result}</#{tag_name}>"
+        end
       end
-    end
-
-    def self.attribute_generation(attributes)
-      return '' if attributes.empty?
-
-      result = attributes.to_a.map { |key, value| " #{key}=\"#{value}\"" }
-      result.join
+  
+      def attribute_generation(attributes)
+        return '' if attributes.empty?
+  
+        result = attributes.to_a.map { |key, value| " #{key}=\"#{value}\"" }
+        result.join
+      end
     end
   end
 end
